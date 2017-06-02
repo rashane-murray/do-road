38c708cd1c6273c0cc869d8848b3e88bed182479
Jhamali <dubzero2221996@hotmail.com>
2017-06-02 14:02:15 -0500

    Pages added to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,20 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { Create } from '../pages/create/create';
 import { SignIn } from '../pages/signIn/signIn';
+import { GooglePlus } from '@ionic-native/google-plus';
+import { NativeStorage } from '@ionic-native/native-storage';
+import {Facebook, FacebookLoginResponse} from '@ionic-native/facebook';
+import { Angular2SocialLoginModule } from 'C:/Users/dubze/Documents/ionic/do-road/node_modules/angular2-social-login';
 
+let p = {
+  "google": {
+      "clientId": "743156431947-d6v8jfh79jrd92j6rgciti7uhln58ic4.apps.googleusercontent.com"
+    },
+     "facebook": {
+      "clientId": "133234047245064",
+      "apiVersion": "v2.9" 
+    }
+}
 @NgModule({
   declarations: [
     MyApp,
@@ -18,7 +31,8 @@ import { SignIn } from '../pages/signIn/signIn';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    Angular2SocialLoginModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -30,7 +44,14 @@ import { SignIn } from '../pages/signIn/signIn';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    GooglePlus,
+    NativeStorage,
+    Facebook
+     
+    
   ]
 })
 export class AppModule {}
+
+Angular2SocialLoginModule.loadProvidersScripts(p);
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 import {Create} from 'C:/Users/dubze/Documents/ionic/do-road/src/pages/create/create';
 import {SignIn} from 'C:/Users/dubze/Documents/ionic/do-road/src/pages/signIn/signIn';
+import {GooglePlus } from '@ionic-native/google-plus';
+import { NativeStorage } from '@ionic-native/native-storage';
+import {Facebook, FacebookLoginResponse} from '@ionic-native/facebook';
+import { AuthService } from 'C:/Users/dubze/Documents/ionic/do-road/node_modules/angular2-social-login';
 
 
 
 @Component({
 selector: 'page-home',
-templateUrl: 'home.html'
+templateUrl: 'home.html',
+
 })
 
 export class HomePage {
 
-constructor(public navCtrl: NavController) {
+sub;
+
+constructor(public navCtrl: NavController, public loadingCtrl: LoadingController, public googlePlus: GooglePlus, public nativeStorage: NativeStorage, public fb: Facebook, public auth: AuthService) {
 
 }
 
@@ -28,9 +35,45 @@ constructor(public navCtrl: NavController) {
   this.navCtrl.push(SignIn);
   }
 
+  doGoogleLogin(){
+  
+  let loading = this.loadingCtrl.create({
+    content: 'Please wait...'
+  });
+  loading.present();
 
-	
+  this.googlePlus.login({})
+  .then( res => {
+  loading.dismiss();
+  this.navCtrl.push(Create);
 
+  })
+ ;
+}
+
+
+facebookLog(){
+  
+this.fb.login(['public_profile', 'user_friends', 'email'])
+  .then((res: FacebookLoginResponse) => console.log('Logged into Facebook!', res))
+  .catch(e => console.log('Error logging into Facebook', e));
+
+
+
+}
+
+loginSign(p: string){
+
+this.sub= this.auth.login(p).subscribe((data) =>{
+  this.navCtrl.push(Create);
+})
+
+}
+
+  logout(){
+    this.auth.logout().subscribe((data)=>{console.log(data);} 
+    )
+  }
 	
 	
 	
